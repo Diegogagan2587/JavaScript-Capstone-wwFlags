75d9372a3b2b7a7f4e40a7b40a1125118be6cd25
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-28 11:15:24 -0600

    refactor getComments fucntion to return a response

--- src/modules/pop_up_comments_get.js
+++ src/modules/pop_up_comments_get.js
@@ -2,11 +2,10 @@
 const appID = 'yY6kfgXjWJt5yNZEfFYH';
 
 const getComments = async (itemID) => {
-    console.log('----------gettin comments-------------------')
     const urlFromAPI = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appID}/comments?item_id=item${itemID}`;
     const request = await fetch(urlFromAPI);
     const response = await request.json();
-    console.log(response)
+    return response
 }
 
 export { getComments };
