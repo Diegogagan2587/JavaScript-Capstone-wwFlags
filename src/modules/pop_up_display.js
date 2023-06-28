65ca59928e0abc435cb287aee779a85ef955113f
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-27 18:38:40 -0600

    add feature: close pop-up

--- src/modules/pop_up_display.js
+++ src/modules/pop_up_display.js
@@ -1,9 +1,11 @@
 console.log('pop up module is runing')
 
-const popUpCard = `
+const setCardData = (countryName) => {
+    const popUpCard = `
 <div class="card">
+<button type="button" class="close-pop-up">X</button>
 <img src="https://img.freepik.com/free-photo/flat-lay-circular-food-frame_23-2148708223.jpg?w=740&t=st=1687833862~exp=1687834462~hmac=c80cdb82e16bac82d49763c95e1e2913f9abe094690fb691b94d490b643b3ada" alt="description here">
-<h3>Name or Title</h3>
+<h3>${countryName}</h3>
 <div class="characteristics">
     <span class="element">Fuel: Titanium</span>
     <span class="element">Length: 100, 000</span>
@@ -11,16 +13,28 @@ const popUpCard = `
     <span class="element">Power: 100,000, 000</span>
 </div>
 </div>
-`
+`;
+return popUpCard
+}
 
 const popUpFather = document.querySelector('.pop-up')
 
-const displayPopUp = () => {
+const displayPopUp = (event) => {
     console.log('runing displayPopUp')
-    console.log('we need to remove hide from ', popUpFather);
+
+    const cardContainer = event.target.parentNode.parentNode;
+    const countryName = cardContainer.querySelector('.conutryName').innerText;
+    const popUpCard = setCardData(countryName);
+    
     popUpFather.innerHTML = popUpCard;
+
+    const closePopUpButton = popUpFather.querySelector('.close-pop-up')
+    
     popUpFather.classList.remove('hide');
     
+    closePopUpButton.addEventListener('click',() => {
+        popUpFather.classList.add('hide');
+    }) 
 }
 
-export {displayPopUp, popUpCard, popUpFather } ;
+export {displayPopUp, setCardData} ;
