65836536f71a91351cf148a7c12a1fcac2fc9832
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-28 12:40:37 -0600

    Add form to submmit comments

--- src/modules/pop_up_display.js
+++ src/modules/pop_up_display.js
@@ -1,30 +1,34 @@
 const setCardData = (countryName, imgUrl = '#', id, area, subRegion, population) => {
   const popUpCard = `
 <div class="card">
-<button type="button" class="close-pop-up">X</button>
-<img src="${imgUrl}" alt="description here">
-<h3>${countryName}</h3>
-<div class="characteristics">
-    <span class="element">${id}</span>
-    <span class="element">${area}</span>
-    <span class="element">${subRegion}</span>
-    <span class="element">${population}</span>
-</div>
-<div class="comments-box">
-  <h3>Comments</h3>
-  <ul class="comments-list">
-    <li>
-        <span class="date">03/11/2021</span>
-        <span class="user">Alex Marin :</span>
-        <span class="comment">I'd love to buty it!</span>
-    </li>
-    <li>
-    <span class="date">03/12/2021</span>
-    <span class="user"> Mia Maring :</span>
-    <span class="comment">I'd love  it!</span>
-</li>
-  <ul>
-<div>
+  <button type="button" class="close-pop-up">X</button>
+  <img src="${imgUrl}" alt="description here">
+  <h3>${countryName}</h3>
+
+  <div class="characteristics">
+      <span class="element">${id}</span>
+      <span class="element">${area}</span>
+      <span class="element">${subRegion}</span>
+      <span class="element">${population}</span>
+  </div>
+
+  <div class="comments-box">
+    <h3>Comments</h3>
+    <ul class="comments-list">
+      <li>
+          <span class="date">03/11/2021</span>
+          <span class="user">Alex Marin :</span>
+          <span class="comment">I'd love to buty it!</span>
+      </li>
+    </ul>
+  </div>
+
+  <form class="add-comments">
+      <input type="text" placeholder="Your name">
+      <textarea placeholder="Your Insights"></textarea>
+      <button type="button">Comment</button type="button">
+  </form>
+
 </div>
 `;
   return popUpCard;
