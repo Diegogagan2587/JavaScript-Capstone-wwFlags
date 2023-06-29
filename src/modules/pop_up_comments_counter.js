5fa87b244477487367c5f71912e6dbc9289269ac
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-28 21:01:01 -0600

    modify commentsCounter() to return 0 if no <li> elements

--- src/modules/pop_up_comments_counter.js
+++ src/modules/pop_up_comments_counter.js
@@ -2,6 +2,7 @@ const commentsCounter = () => {
     console.log('runing commentsCounter');
     let result = 0;
     const comments = document.querySelectorAll('.comments-list li');
+    if(!comments){ return 0 }
     result = comments.length;
     console.log(result)
     return result;
