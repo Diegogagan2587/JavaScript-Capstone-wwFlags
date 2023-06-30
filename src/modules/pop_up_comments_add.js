638a88790337030edc305d12096fa301d6c4a42a
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-30 14:07:36 -0600

    Improves Feature: Now submitted comments will dislpay inmedeatly after submission, removes function to reaload website after submiting

--- src/modules/pop_up_comments_add.js
+++ src/modules/pop_up_comments_add.js
@@ -2,7 +2,7 @@ const appID = 'yY6kfgXjWJt5yNZEfFYH';
 
 const commentsURL = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appID}/comments`;
 
-const postComments = async (itemID, user, comment) => {
+const postComments = async (itemID, user, comment , container) => {
   const request = await fetch(commentsURL, {
     method: 'POST',
     body: JSON.stringify({
@@ -15,7 +15,14 @@ const postComments = async (itemID, user, comment) => {
     },
   });
   const response = request.status;// status should be 201 wich means comment was created
-  window.location.reload();
+  const currentDate = 'Today';
+  const newComment = document.createElement('li');
+  newComment.innerHTML = `
+  <span class="date">${currentDate}</span>
+  <span class="user">${user} :</span>
+  <span class="comment">${comment}</span>
+  `;
+  container.appendChild(newComment);
   return response;
 };
 
